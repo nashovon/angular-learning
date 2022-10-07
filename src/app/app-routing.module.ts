c4110e6686a22e6691fd5c0caa581cc4b9b73404
shovon <neamulahasan@gmail.com>
2022-10-08 02:32:25 +0600

    repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {NewCompComponent} from "./new-comp/new-comp.component";
 
+// sets up routes constant where you define your routes
 const routes: Routes = [
   {path: 'new-comp-url' , component:NewCompComponent},
 ];
